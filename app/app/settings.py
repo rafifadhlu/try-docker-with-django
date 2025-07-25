d3b43635e2380ea5d7c879c8f0344a25e7071768
rafifadhlu <rafifadhlu11@gmail.com>
2025-07-25 14:03:31 +0700

    [update] Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,11 @@ SUPABASE_BUCKET = os.getenv('SUPABASE_BUCKET')
 DEBUG = DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
-    "try-docker-with-django-production.up.railway.app"
+    os.getenv("ALLOWED_HOST", "127.0.0.1")
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://try-docker-with-django-production.up.railway.app"
 ]
 
 
